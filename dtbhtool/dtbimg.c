7fcb81bc2084bb1527264f8c2b2941d395f47006
gimme <gimmeitorilltell@gmail.com>
2018-01-30 17:36:42 -0500

    dtbimg: Add device file from zerofltespr here until we fix symlinks

--- dtbhtool/dtbimg.c
+++ dtbhtool/dtbimg.c
@@ -35,7 +35,28 @@
 #include <stdint.h>
 
 /* must be provided by the device tree */
-#include <samsung_dtbh.h>
+/* fix this after the link are working properly */
+/* until then I will add my device file here */
+#ifndef _SAMSUNG_DTBH_H_
+#define _SAMSUNG_DTBH_H_
+
+#define DTBH_MAGIC         "DTBH"
+#define DTBH_VERSION       2
+#define DTBH_PLATFORM      "android"
+#define DTBH_SUBTYPE       "samsung"
+ /* Hardcoded entry */
+#define DTBH_PLATFORM_CODE 0x000050a6
+#define DTBH_SUBTYPE_CODE  0x217584da
+
+/* DTBH_MAGIC + DTBH_VERSION + DTB counts */
+#define DT_HEADER_PHYS_SIZE 12
+
+/*
+ * keep the eight uint32_t entries first in this struct so we can memcpy them to the file
+ */
+#define DT_ENTRY_PHYS_SIZE (sizeof(uint32_t) * 8)
+
+#endif // _SAMSUNG_DTBH_H_
 
 #include "libfdt.h"
 
